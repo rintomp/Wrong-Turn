9633555c7ed122f0f9e6eefa4a904f48a4911d96
Rinto M Prince <89894914+rintomp@users.noreply.github.com>
2024-05-19 22:42:22 +0530

    Update wrongturn.sh

--- wrongturn.sh
+++ wrongturn.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-
+# Generate ASCII art text
 toilet -f bigmono9 -F gay WR0NG TURN
 
-echo -e ☠️..☠️..☠️..☠️..."\e[31mW\e[0m"R0NG "\e[31mT\e[0m"URN...☠️..☠️..☠️..☠️"\n\nA simple straightforward tool; enter the target URL (example, testphp.vulnweb.com).\n"
-echo "
- Enter URL : "
+# Print the warning message
+echo -e "☠️..☠️..☠️..☠️...\e[31mW\e[0mR0NG \e[31mT\e[0mURN...☠️..☠️..☠️..☠️"
+echo -e "\n\nA simple straightforward tool; enter the target URL (example, testphp.vulnweb.com).\n"
+echo " Enter URL : "
+
+# Read the URL from user input
 read url
 
-waybackurls $url | grep -a -i \=http | qsreplace 'http://evil.com' | while read host do;do curl -s -L  $host -I|grep "evil.com" && echo -e "$host \e[41mWr0ng Turn\e[0m☠️" ;done
+# Fetch URLs from the Wayback Machine, filter, replace and check
+waybackurls $url | grep -a -i \=http | qsreplace 'http://evil.com' | while read host; do
+    curl -s -L $host -I | grep "evil.com" && echo -e "$host \e[41mWr0ng Turn\e[0m☠️"
+done
 
-echo -e "\n\e[34mCopy and test Wr0ng Turn results on browser to avoid false positive
- \e[0m"
-
-
+# Final message
+echo -e "\n\e[34mCopy and test Wr0ng Turn results on browser to avoid false positives\e[0m"
