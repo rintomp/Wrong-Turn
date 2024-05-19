a389e3115691d74fdd282b09cd1d7fc8b9896ca8
Rinto M Prince <89894914+rintomp@users.noreply.github.com>
2024-05-19 22:41:15 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,14 +1,51 @@
-sudo apt-get install toilet
-sudo apt install golang
-sudo apt install
-sudo go install github.com/tomnomnom/waybackurls@latest
-sudo go install github.com/tomnomnom/qsreplace@latest
-chmod +x wrongturn.sh
-sudo cp go/bin/waybackurls /usr/local/bin
-sudo cp go/bin/qsreplace /usr/local/bin
+#!/bin/bash
 
-sudo cp go/bin/waybackurls /usr/local/bin
-sudo cp go/bin/qsreplace /usr/local/bin
+# Function to check and install dependencies
+install_dependency() {
+    local dep=$1
+    local install_cmd=$2
 
-#sudo cp /root/go/bin/waybackurls /usr/local/go/bin/
-#sudo cp /root/go/bin/qsreplace /usr/local/go/bin/
+    if ! command -v $dep &> /dev/null
+    then
+        echo "$dep could not be found, installing..."
+        eval $install_cmd
+    else
+        echo "$dep is already installed."
+    fi
+}
+
+# Update package list
+sudo apt-get update
+
+# Install toilet
+install_dependency "toilet" "sudo apt-get install -y toilet"
+
+# Install Go (required for installing Go-based tools)
+install_dependency "go" "sudo apt-get install -y golang"
+
+# Ensure GOPATH is set
+if [ -z "$GOPATH" ]; then
+    export GOPATH=$HOME/go
+    echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+    echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+    export PATH=$PATH:$GOPATH/bin
+else
+    export PATH=$PATH:$GOPATH/bin
+fi
+
+# Source the updated .bashrc to reflect GOPATH changes
+source ~/.bashrc
+
+# Install qsreplace using go install
+install_dependency "qsreplace" "go install github.com/tomnomnom/qsreplace@latest"
+
+# Install waybackurls using go install
+install_dependency "waybackurls" "go install github.com/tomnomnom/waybackurls@latest"
+
+# Install curl
+install_dependency "curl" "sudo apt-get install -y curl"
+
+# Install grep (typically pre-installed on most Unix systems)
+install_dependency "grep" "sudo apt-get install -y grep"
+
+echo "Installation complete. You can now run the tool using ./wrongturn.sh"
